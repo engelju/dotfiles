2800560abd9ed74824d89a51c9bc7420293418ad
Julie Engel <jue@metanet.ch>
2016-03-30 17:13:16 +0200

    add vlc to now_playing script

--- scripts/now_playing.sh
+++ scripts/now_playing.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-NOW_PLAYING=$(osascript <<EOF
+# todo:
+# add mocp
+# cleanup osascript
+
+NOW_PLAYING_DEEZER=$(osascript <<EOF
 set deezer_state to false
 
 if is_app_running("Deezer") then
@@ -21,10 +25,33 @@ on is_app_running(app_name)
 end is_app_running
 EOF)
 
-if [ $NOW_PLAYING -eq 1 ]; then
-    # curl gds.fm
-    GDS_PLAYING=$(curl -s http://gdsfm.airtime.pro/api/live-info-v2 | /usr/local/bin/jq -r '.tracks.current.name')
-    echo "GDS.fm is playing: $GDS_PLAYING"
+NOW_PLAYING_VLC=$(osascript <<EOF
+tell application "System Events"
+	set myList to (name of every process)
+end tell
+
+if myList contains "VLC" then
+	tell application "VLC"
+		if playing then
+			set track_name to name of current item
+			return "VLC is playing: #[bold]" & track_name & "#[nobold]"
+		else
+			return 1
+		end if
+	end tell
 else
-    echo $NOW_PLAYING
+	return 1
+end if
+EOF)
+
+if [ $NOW_PLAYING_DEEZER -eq 1 ]; then
+    if [ $NOW_PLAYING_VLC -eq 1 ]; then
+        # curl gds.fm
+        GDS_PLAYING=$(curl -s http://gdsfm.airtime.pro/api/live-info-v2 | /usr/local/bin/jq -r '.tracks.current.name')
+        echo "GDS.fm is playing: $GDS_PLAYING"
+    else
+        echo $NOW_PLAYING_VLC
+    fi
+else
+    echo $NOW_PLAYING_DEEZER
 fi
